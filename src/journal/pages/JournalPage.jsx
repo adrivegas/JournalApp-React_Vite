3a38f54a3d06691d481b751cef86094647c5d980
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-08 12:58:08 -0500

    Se crea vista NothingSelectedView para cuando no hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,10 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti molestias, incidunt sed qui tempore fuga itaque natus minus laboriosam? Et assumenda dolorum libero earum facere? Maxime velit aut nulla commodi.</Typography>
+      <NothingSelectedView/>
     </JournalLayout>
   )
 }
