7907a84884e8872d1daa6906c403daf5c49c6fa4
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-08 11:21:38 -0500

    Diseño de SideBar component

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
+import { Toolbar } from '@mui/material';
 import { Box } from '@mui/system';
-import { NavBar } from '../components';
+import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 280;
 
@@ -9,13 +10,13 @@ export const JournalLayout = ({ children }) => {
 
         <NavBar drawerWidth={ drawerWidth } />
 
-        {/* <SideBar drawerWidth={ drawerWidth } /> */}
+        <SideBar drawerWidth={ drawerWidth } />
 
         <Box 
             component='main'
             sx={{ flexGrow: 1, p: 3 }}
         >
-            {/* <Toolbar /> */}
+            <Toolbar />
 
             { children }
             
