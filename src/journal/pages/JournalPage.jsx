b90aaced8a1fe55837c7f505e28124a6da634e35
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-08 10:24:11 -0500

    Se implementa JournalLayout en JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,10 @@
 import { Typography } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti molestias, incidunt sed qui tempore fuga itaque natus minus laboriosam? Et assumenda dolorum libero earum facere? Maxime velit aut nulla commodi.</Typography>
+    </JournalLayout>
   )
 }
