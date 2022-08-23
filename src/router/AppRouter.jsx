90bdf32e4b567147414ecb9d8fdec7b07e231b07
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-23 13:48:20 -0500

    Se crea un custom hook que está pendiente de la autenticación y se realiza protección de rutas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,36 @@
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
-import { CheckingAuth } from '../ui';
+import { CheckingAuth } from '../ui/';
+import { useCheckAuth } from '../hooks';
+
 
 export const AppRouter = () => {
 
-    const status = useSelector(state => state.auth);
+    const status = useCheckAuth();
 
-    if ( status === 'checking' ) {
-      return <CheckingAuth />
+    if (status === 'checking') {
+        return <CheckingAuth />
     }
 
     return (
         <Routes>
-            {/* Login y Registro */}
-            <Route path="/auth/*" element={<AuthRoutes />} />
 
-            {/* JournaApp */}
-            <Route path="/*" element={<JournalRoutes />} />
+            {
+                (status === 'authenticated')
+                    ? <Route path="/*" element={<JournalRoutes />} />
+                    : <Route path="/auth/*" element={<AuthRoutes />} />
+            }
+
+            <Route path='/*' element={<Navigate to='/auth/login' />} />
+
+            {/* Login y Registro */}
+            {/* <Route path="/auth/*" element={ <AuthRoutes /> } /> */}
+
+            {/* JournalApp */}
+            {/* <Route path="/*" element={ <JournalRoutes /> } /> */}
+
         </Routes>
     )
 }
