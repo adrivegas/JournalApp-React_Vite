795b2842d65dde86d1370f888863c082c93788a1
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-25 14:32:39 -0500

    Mostrar notas en el menú lateral

---
+++ src/journal/components/SideBarItem.jsx
@@ -0,0 +1,30 @@
+import { useMemo } from 'react'
+import { useDispatch } from 'react-redux'
+import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { TurnedInNot } from '@mui/icons-material';
+import { setActiveNote } from '../../store/journal';
+
+
+export const SideBarItem = ({ note }) => {
+
+  
+    const newTitle = useMemo(() => {
+        return title.length > 17
+            ? title.substring(0, 17) + '...'
+            : title;
+    }, [title])
+
+    return (
+        <ListItem disablePadding>
+            <ListItemButton>
+                <ListItemIcon>
+                    <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                    <ListItemText primary={ newTitle } />
+                    <ListItemText secondary={ body } />
+                </Grid>
+            </ListItemButton>
+        </ListItem>
+    )
+}
