9db66e4470e14f54804c001e9c4bc7f6f41b47a8
Adriana Venegas <adrianne.venegas@gmail.com>
2022-08-29 11:35:35 -0500

    Seleccionar files desde react, se usa useRef para simular el click en input desde botón de subida

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { SaveOutlined } from '@mui/icons-material';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { SaveOutlined, UploadOutlined } from '@mui/icons-material';
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
 import Swal from 'sweetalert2';
 import 'sweetalert2/dist/sweetalert2.css';
 import { useForm } from '../../hooks/useForm';
@@ -36,6 +36,14 @@ export const NoteView = () => {
         dispatch(startSaveNote());
     }
 
+    const onFileInputChange = ({target}) => {
+        console.log(target.files);
+        if( target.files ) return;
+        console.log('Subiendo archivos');
+        // dispatch( startUploadingFiles( target.files ));
+
+    };
+
     return (
         <Grid
             className='animate__animated animate__fadeIn animate__faster'
@@ -48,6 +56,21 @@ export const NoteView = () => {
                 <Typography fontSize={39} fontWeight='light' >8 de agosto, 2022</Typography>
             </Grid>
             <Grid item>
+                <input
+                    type="file"
+                    multiple
+                    ref={ fileInputRef }
+                    onChange={ onFileInputChange }
+                    style={{ display: 'none' }}
+                />
+
+                <IconButton
+                    color="primary"
+                    disabled={ isSaving }
+                    onClick={() => fileInputRef.current.click()}
+                >
+                    <UploadOutlined />
+                </IconButton>
                 <Button
                     disabled={isSaving}
                     onClick={onSaveNote}
